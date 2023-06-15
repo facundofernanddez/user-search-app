ad029b09094dbfdfd06d74b2db4bb6ebb3b91d85
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-06-15 07:36:15 -0300

    changes names

--- src/components/icons/MoonIcon.tsx
+++ src/components/icons/MoonIcon.tsx
@@ -1,6 +1,6 @@
 import { SVGProps } from "react";
 
-export const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
+export const MoonIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     height="1em"
