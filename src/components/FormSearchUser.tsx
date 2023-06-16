d0a8bb61e6f5f8e13e3cc5eeaea2cdf609cfe9f2
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-06-16 07:08:54 -0300

    create github icon and improve navbar

--- src/components/FormSearchUser.tsx
+++ src/components/FormSearchUser.tsx
@@ -2,7 +2,7 @@ import { SearchIcon } from "@/components/icons/SearchIcon";
 
 export const FormSearchUser = () => {
   return (
-    <form className="flex flex-wrap gap-2 items-center bg-blue-900 p-2 rounded-xl">
+    <form className="flex flex-wrap gap-2 items-center bg-blue-900 p-2 rounded-xl mb-6">
       <span className="min-w-[20px]">
         <SearchIcon className="fill-blue-500" />
       </span>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
+import { SunIcon } from "./icons/SunIcon";
+
 export const Navbar = () => {
   return (
-    <header>
-      <h1>devfinder</h1>
-      <div>
-        <span className="uppercase">Light</span>
-        <button></button>
-      </div>
+    <header className="flex items-center space-x-2 mb-10">
+      <h1 className="text-white text-4xl font-bold flex-grow">devfinder</h1>
+      <span className="uppercase text-white">Light</span>
+      <button>
+        <SunIcon className="fill-white" />
+      </button>
     </header>
   );
 };
