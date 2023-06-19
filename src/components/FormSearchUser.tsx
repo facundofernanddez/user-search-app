67087c16f7d2b0d7e2947201bcee0d99e69d42b5
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-06-18 22:27:38 -0300

    use client form component

--- src/components/FormSearchUser.tsx
+++ src/components/FormSearchUser.tsx
@@ -1,12 +1,22 @@
+"use client";
 import { SearchIcon } from "@/components/icons/SearchIcon";
 
 export const FormSearchUser = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(e.currentTarget.username.value);
+  };
+
   return (
-    <form className="flex flex-wrap gap-2 items-center bg-blue-900 p-4 rounded-xl mb-6">
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-wrap gap-2 items-center bg-blue-900 p-4 rounded-xl mb-6"
+    >
       <span className="min-w-[20px]">
         <SearchIcon className="fill-blue-500" />
       </span>
       <input
+        name="username"
         type="text"
         placeholder="Search Github username..."
         className="flex-1 h-14 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 bg-transparent text-white placeholder-white"
