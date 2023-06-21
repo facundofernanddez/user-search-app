3d239dfcc21dcdf8f2a47a1671ec26454b4333aa
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-06-21 07:52:34 -0300

    user not found message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,23 +7,28 @@ import { User } from "@/interfaces/user";
 
 const Home = () => {
   const [user, setUser] = useState<User | null>(null);
+  const [error, setError] = useState<string | null>(null);
 
   const getUser = async (username: string) => {
     const res = await fetch(`https://api.github.com/users/${username}`);
 
     if (!res.ok) {
       setUser(null);
+      setError("User not found");
       return;
     }
 
     setUser(await res.json());
+    setError(null);
   };
 
   return (
     <>
       <FormSearchUser getUser={getUser} />
       {user && <UserCardInfo user={user} />}
-      {/* <UserCardInfo user={user} /> */}
+      {error && (
+        <div className="rounded-lg bg-red-500 p-4 text-white">{error}</div>
+      )}
     </>
   );
 };
